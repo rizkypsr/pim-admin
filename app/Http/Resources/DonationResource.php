f7049cec86256d27457dc8b79afb0c80e6b019f4
rizkypsr <rizkypsrr@gmail.com>
2024-06-15 07:57:35 +0700

    feat: add faq to donation

--- app/Http/Resources/DonationResource.php
+++ app/Http/Resources/DonationResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Helpers\WhatsappFormat;
+use App\Models\Faq;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,12 +16,16 @@ class DonationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $faq = Faq::where('name', 'wa')->first();
+
         return [
             'id' => $this->id,
             'title' => $this->title,
             'subtitle' => $this->subtitle,
             'description' => $this->description,
             'video' => $this->video,
+            'whatsapp_number' => $faq->value ?? null,
+            'whatsapp_url' => $faq->value ? WhatsappFormat::format($faq->value) : null,
             'images' => DonationImageResource::collection($this->donationImages),
             'created_at' => $this->created_at,
         ];
