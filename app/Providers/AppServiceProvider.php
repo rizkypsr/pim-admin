6225ca852c44dc183fe8193760c605e723125e02
rizkypsr <rizkypsrr@gmail.com>
2024-07-08 11:51:09 +0700

    fix: add pulse gate middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -23,7 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::define('viewPulse', function (User $user) {
-            return $user->isAdmin();
+            // check if user logged in
+            return Auth::check();
         });
 
         Blade::directive('rupiah', function ($expression) {
