5bd13a96509c77f8a8168868621e5ac008afdf6f
rizkypsr <rizkypsrr@gmail.com>
2024-07-16 16:40:19 +0700

    fix

--- resources/views/share/index.blade.php
+++ resources/views/share/index.blade.php
@@ -11,6 +11,18 @@
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Pusat Info Mobil</title>
+
+    <script>
+        let text = {{ $shortURL }}
+        const copyContent = async () => {
+            try {
+                await navigator.clipboard.writeText(text);
+                console.log('Content copied to clipboard');
+            } catch (err) {
+                console.error('Failed to copy: ', err);
+            }
+        }
+    </script>
 </head>
 
 <body>
@@ -63,18 +75,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-
-    <script>
-        let text = {{ $shortURL }}
-        const copyContent = async () => {
-            try {
-                await navigator.clipboard.writeText(text);
-                console.log('Content copied to clipboard');
-            } catch (err) {
-                console.error('Failed to copy: ', err);
-            }
-        }
     </script>
 </body>
 
